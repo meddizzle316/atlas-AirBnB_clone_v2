cfb1ddb4fc9a0f8ea77451208d533a211921ee8c
meddizzle316 <meddizzle316@gmail.com>
2024-03-08 12:51:40 -0600

    tyring something out

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,9 +24,11 @@ def c(text):
     return f"C {safe_text}"
 
 
-# @app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text='is cool'):
+@app.route("/python", strict_slashes=False)
+# @app.route("/python/<text>", strict_slashes=False)
+# def python(text='is cool'):
+@app.route("/python/<text>", defaults={'text':'is cool'}, strict_slashes=False)
+def python(text):
     safe_text = escape(text)
     safe_text = safe_text.replace("_", " ")
     return f"Python {safe_text}"
