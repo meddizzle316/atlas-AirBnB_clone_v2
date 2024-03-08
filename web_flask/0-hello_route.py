24dd04c51cbc9db45aec751025e0de6c59279b34
meddizzle316 <meddizzle316@gmail.com>
2024-03-08 11:49:35 -0600

    0-hello and readme changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api/hello')
+@app.route('/', strict_slashes=False)
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
