855d259da6998b8b741ab86dbf1cb66715362f9c
meddizzle316 <meddizzle316@gmail.com>
2024-03-08 12:29:23 -0600

    add 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""frigging documentation for module for dynamic routes"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    safe_text = escape(text)
+    safe_text = safe_text.replace("_", " ")
+    return f"C {safe_text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text='is cool'):
+    safe_text = escape(text)
+    safe_text = safe_text.replace("_", " ")
+    return f"Python {safe_text}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    if isinstance(n, int):
+        return n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
