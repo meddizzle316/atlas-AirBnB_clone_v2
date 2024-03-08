01814ec2e17d5f6b2f5ae9765c4022e5d6b2712d
meddizzle316 <meddizzle316@gmail.com>
2024-03-08 11:51:24 -0600

    py fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,11 +2,13 @@
 """freaking documentation for basic flask app"""
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
