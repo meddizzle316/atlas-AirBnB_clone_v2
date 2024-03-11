dbbfa70cac0711c17140e19909e6915717049afe
meddizzle316 <meddizzle316@gmail.com>
2024-03-11 20:22:27 +0000

    I think I might have nine close

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""frigging documentation for module for dynamic routes"""
+from flask import Flask, abort, render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def states_list():
+    all_states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', items=all_states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
