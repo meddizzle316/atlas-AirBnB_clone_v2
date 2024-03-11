3fbe4bb97beff31f1d5c82425514a19efaf4d743
meddizzle316 <meddizzle316@gmail.com>
2024-03-11 18:49:17 +0000

    attempt at 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,11 +11,11 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    all_states = storage.all(State)
+    all_states = storage.all(State).values()
     return render_template('7-states_list.html', items=all_states)
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exception):
     storage.close()
 
 
